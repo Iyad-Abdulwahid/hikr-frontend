6814f3cab5d94d0b4183b98996c71d81e06e22ef
iyad-abdulwahid <iyadabdulwahid23@gmail.com>
2022-08-08 17:10:54 +0000

    test

--- hikr-frontend/src/components/Region.js
+++ hikr-frontend/src/components/Region.js
@@ -43,17 +43,3 @@ return (
   
 }
 export default Region;
-
-//ok, what do I do here?
-//need to grab the hikes from the database and set them to state
-//for each one need to create a div with a description of the hike, possibly a picture?
-//each one needs to have an edit button, when the edit button is clicked it brings up the edit paged
-//the edit page will have a button that allows users to delete the hike, and a form to edit a specific portion of the hike
-//the bottom of the page will have a link that brings people to a add hike page
-
-
-//add a hikes component that sits in the regions component - contains detailed info on the hike and the ability to edit/delete 
-
-
-//decide upon regions, what states they include, and throw a region property into hike schema
-//add images as a property  
